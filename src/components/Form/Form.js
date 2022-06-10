fad0e78fee6b0974ce70ef261d9216b7462c8993
Ismaeel Abdulghani <ismaeelaa849@gmail.com>
2022-06-09 22:30:40 -0400

    Finished contact form front end

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,7 +5,8 @@ class Form extends React.Component {
   constructor(props){
       super(props);
       this.state = {
-          name: '',
+          fName: '',
+          lName: '',
           email: '',
           message: '',
           disabled: false,
@@ -15,16 +16,30 @@ class Form extends React.Component {
   
   handleSubmit =(event)=>{
       event.preventDefault()
-    console.log('Yo')
+
+      this.setState({
+          disabled: true,
+      })
+    }
+    handleChange = (event)=>{
+        const target = event.target
+        const id = target.id
+
+        this.setState({
+            [id]: target.value
+        })
+        
     }
 
   render(){
       return(
-        <form className="Items" onSubmit={this.handleSubmit}>
-           <div> <input type="text" placeholder="First Name"  onChange={this.handleChange}/></div>
-            <div><input type="text" placeholder="Last Name"  onChange={this.handleChange} /></div>
-            <div><input type="email" placeholder="Email"  onChange={this.handleChange} /></div>
-           <div> <input type="text" placeholder="Message"  onChange={this.handleChange} /></div>
+          
+        <form className="items" onSubmit={this.handleSubmit}>
+            <p>If you want to reach out to me, fill out this form, and I will get back to you as soon as possible.</p>
+           <div> <input type="text" id="fName" placeholder="First Name"  onChange={this.handleChange}/></div>
+            <div><input type="text" id="lName" placeholder="Last Name"  onChange={this.handleChange} /></div>
+            <div><input type="email" id="email" placeholder="Email"  onChange={this.handleChange} /></div>
+           <div> <textarea id="message" placeholder="Message"  onChange={this.handleChange} /></div>
             <div><input type="submit"/></div>
         </form>
       )
