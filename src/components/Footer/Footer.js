ab7fcbdef3cf3397c921554d798378cd024e3878
Ismaeel Abdulghani <ismaeelaa849@gmail.com>
2022-08-04 11:27:05 -0400

    Added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,10 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <div className='bottom'>Ismaeel Abdulghani</div>
+    <div className='bottom'>
+      <a href='https://github.com/iaAbdulghani'>Github</a>
+      <a href='https://www.linkedin.com/in/ismaeelabdulghani/'>Linkedin</a>
+      
+      </div>
   )
 }
