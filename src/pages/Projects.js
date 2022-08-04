f1c06a208e0399e65afd928e3f0a4f54f6973ec4
Ismaeel Abdulghani <ismaeelaa849@gmail.com>
2022-08-03 21:40:07 -0400

    Added project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Item from '../components/Item/Item'
+import firstImage from '../pictures/LetMeGuess.png'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <span>
+      <div >
+        <Item name={"Let Me Guess"} place={<a href="https://github.com/iaAbdulghani/Akinator-Clone">Github</a>} picture={firstImage} description={"My first project. Using Java, I stored lots of different characters in a data structure and assigned different attributes to them. Then I created a UI using Java Swing that asked the user randomized yes/no questions to narrow the characters down to one."}/>
+        
+        
+      </div>
+      </span>
   )
 }
